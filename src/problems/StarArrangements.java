d8691c22a42b44635576fbbe398be9b01505bba5
unknown <annagoclowska92@gmail.com>
2019-02-12 21:42:07 +0000

    problems with difficulty 1.4

---
+++ src/problems/StarArrangements.java
@@ -0,0 +1,23 @@
+package problems;
+
+import java.util.Scanner;
+
+public class StarArrangements {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		System.out.println(num + ":");
+		for (int i=2; i<num; i++) {
+			if (num % (2*i -1)==0 || num % (2*i-1)==i) {
+				System.out.println(i +", "+ (i-1));
+			}
+			
+			if (num % (2*i)==i || num % (2*i) ==0) {
+				System.out.println(i +", "+ i);
+			}
+		}
+
+	}
+
+}
